17f034d132d3d8ad63126a35d8c4a5aeee26dca4
napat  srichantapong <tot14883@gmail.com>
2017-06-26 22:55:36 +0700

    FoodRandom

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FoodRandom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 310);
+            this.button1.MaximumSize = new System.Drawing.Size(75, 23);
+            this.button1.MinimumSize = new System.Drawing.Size(75, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "สุ่ม";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "เมนูที่สุ่มได้";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(334, 235);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 345);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "FoodRandom";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodRandom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i;
+                String[] Manu = { "", "กระเพราะไก่", "ข้าวผัด", "ข้าวหมูทอด", "ข้าวไข่เจียว", "ก๋วยเตี๋ยวต้มยำ", "ส้มตำ", "ขนมจีน", "คอหมูย่างราดข้าว", "ไก่ทอด", "พิซซ่า" };
+                Random ran = new Random();
+                int a = ran.Next(1, 11);
+                for (i = 1; i <= 10; i++)
+                {
+                    String x = Manu[i];                                        
+                }
+                switch (a)
+                {
+                    case 1: label3.Text = "1 : " + Manu[1]; break;
+                    case 2: label3.Text = "2 : " + Manu[2]; break;
+                    case 3: label3.Text = "3 : " + Manu[3]; break;
+                    case 4: label3.Text = "4 : " + Manu[4]; break;
+                    case 5: label3.Text = "5 : " + Manu[5]; break;
+                    case 6: label3.Text = "6 : " + Manu[6]; break;
+                    case 7: label3.Text = "7 : " + Manu[7]; break;
+                    case 8: label3.Text = "8 : " + Manu[8]; break;
+                    case 9: label3.Text = "9 : " + Manu[9]; break;
+                    case 10: label3.Text = "10 : " + Manu[10]; break;
+                }
+            }
+            catch
+            {
+
+            }      
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] Manu = { "", "กระเพราะไก่", "ข้าวผัด", "ข้าวหมูทอด", "ข้าวไข่เจียว", "ก๋วยเตี๋ยวต้มยำ", "ส้มตำ", "ขนมจีน", "คอหมูย่างราดข้าว", "ไก่ทอด", "พิซซ่า" };
+            for (int i = 1; i <= 10; i++)
+            {
+                richTextBox1.Text += i + ": " + Manu[i]+"\n";
+            }
+        }
+    }
+}
